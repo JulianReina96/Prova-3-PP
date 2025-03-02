2fbd7212d62933134c5309b5f04340c627948e7e
julian <julianreina@gmail.com>
2025-03-01 21:34:27 -0300

    --commit wpp strategy

--- src/br/edu/ifba/inf011/model/strategy/WhatsappStrategy.java
+++ src/br/edu/ifba/inf011/model/strategy/WhatsappStrategy.java
@@ -1,17 +1,47 @@
 package br.edu.ifba.inf011.model.strategy;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 import br.edu.ifba.inf011.model.evento.Evento;
 
 public class WhatsappStrategy implements FormaEnvioStrategy {
-    @Override
-    public String formatarMensagem(Evento e) {
-        return "Mensagem formatada para WhatsApp: " + e.getDescricao();
+	
+	private List<Predicate<Evento>> condicoes;
+	private FormaEnvioStrategy proxima;
+	private boolean adicionarAoGoogleCalendar;
+	
+	public WhatsappStrategy(List<Predicate<Evento>> condicoes, FormaEnvioStrategy proxima, boolean adicionarAoGoogleCalendar) {
+        this.condicoes = condicoes;
+        this.proxima = proxima;
+        this.adicionarAoGoogleCalendar = adicionarAoGoogleCalendar;
     }
-
     @Override
-    public void enviarMensagem(String mensagem) {
+    public String formatarMensagem(String mensagem) {
+        return "Mensagem formatada para WhatsApp: " + mensagem;
+    }
+     
+    @Override
+	public boolean deveAdicionarAoGoogleCalendar() {	
+		return adicionarAoGoogleCalendar;
+	}
+    @Override
+    public void enviarMensagem(Evento evento) {
+    	if (deveAplicar(evento)) {
+    		String mensagem = this.formatarMensagem(evento.getDescricao());  		
+    		
         System.out.println("Enviando WhatsApp: " + mensagem);
+        }
+    	if (proxima != null) {
+            proxima.enviarMensagem(evento);
+        }
     }
+    
+    @Override
+    public boolean deveAplicar(Evento evento) {
+        return condicoes.stream().anyMatch(condicao -> condicao.test(evento));
+    }
+	
 }
 
 
